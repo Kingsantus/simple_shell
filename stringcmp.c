3bffb03d13332a0d5008c0aa4657fb352b32ee03
kingsantus <nkingsantus@gmail.com>
2023-04-28 19:24:01 -0700

    simple shell

---
+++ stringcmp.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+* _strchr - Locates a character in a string.
+*
+* Return: If c is found - a pointer to the first occurence.
+*/
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index]; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return (NULL);
+}
+
+/**
+* _strspn - Gets the length of a prefix substring.
+*
+* Return: The number of bytes in s which
+*/
+int _strspn(char *s, char *accept)
+{
+	int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+		}
+		s++;
+	}
+	return (bytes);
+}
+
+/**
+* _strcmp - Compares two strings.
+*
+* Return: Positive byte difference if s1 > s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+
+	return (0);
+}
+
+/**
+* _strncmp - Compare two strings.
+*
+* Return: Less than 0 if s1 is shorter than s2.
+*/
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	{
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+	}
+	if (i == n)
+		return (0);
+	else
+		return (-15);
+}
